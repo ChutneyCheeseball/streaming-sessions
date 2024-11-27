a8ad506a719fd6caa2de5251df2d308fa723a306
Dylan <dylan.du.toit@gmail.com>
2024-11-27 21:43:03 +0200

    Groundwork for API integration

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { Session } from "./classes/Session";
 import { Sessions } from "./classes/Sessions";
-import { Button, Container, Slider, Stack } from "@mui/material";
-import { getMarks, SessionDialog } from "./components/SessionDialog";
+import { Button, Container, Stack } from "@mui/material";
+import { SessionDialog } from "./components/SessionDialog";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { SessionList } from "./components/SessionList";
+import axios from "axios";
 
 function App() {
   const [sessions, setSessions] = useState(new Sessions());
@@ -17,6 +18,7 @@ function App() {
     newSessions.add(session);
     setSessions(newSessions);
     setShowSessionDialog(false);
+    postSession(session);
   };
 
   // useEffect(() => {
@@ -42,6 +44,32 @@ function App() {
     setSessions(newSessions);
   };
 
+  const getSessions = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/sessions");
+      if (response.status === 200) {
+        let newSessions = new Sessions();
+        for (let s of response.data) {
+          newSessions.addNew(s.id, s.start, s.end);
+        }
+        setSessions(newSessions);
+      } else {
+        console.log("Oh no");
+      }
+    } catch (e) {
+      console.log("Oh no");
+    }
+  };
+
+  const postSession = async (session: Session) => {
+    try {
+      const response = await axios.post("http://localhost:3001/sessions", session);
+      console.log(response.status);
+    } catch (e) {
+      console.log("Oh no");
+    }
+  };
+
   return (
     <div className="App">
       {showSessionDialog && (
@@ -56,6 +84,9 @@ function App() {
         <SessionList sessions={sessions} onEdit={handleEditSession} onDelete={handleDeleteSession} />
         <Button variant="contained" color="primary" startIcon={<AddCircleIcon />} onClick={handleAddSession}>
           Add New Session
+        </Button>
+        <Button variant="contained" color="primary" onClick={getSessions}>
+          GET Sessions
         </Button>
       </Container>
     </div>
